70c13182f9d88375983031c4c727d7a99b1a5388
faridaff <faridrafi029@email.com>
2023-11-20 12:16:51 +0700

    Initial commit

---
+++ app/src/main/java/com/example/multilayout123/SecondActifity.kt
@@ -0,0 +1,11 @@
+package com.example.multilayout123
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondActifity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second_actifity)
+    }
+}
